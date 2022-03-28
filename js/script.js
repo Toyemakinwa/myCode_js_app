fc972370e4ab8687e1cf650ef79a480e916bdde5
Toyemakinwa <toyemakinwa@yahoo.com>
2022-03-28 09:45:48 +0300

    Making Pokemons
    If added, this should create a list of pokemons.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-alert('Hello World');
-let favoriteFood = 'rice';
-document.write(favoriteFood);
+<script>
+    // creating pokemons inside a pokemon list; characters description from pokedex.org
+
+    let pokemonList =  [
+        {name: 'Fearrow', height: 1.2, type: ['water', 'speed']}, 
+        {name: 'Ivysaur', height: 1, type: ['grass', 'poison']}, 
+        {name: 'Charmeleon', height: 1.1, type: ['love', 'weather']}];
+
+</script>
