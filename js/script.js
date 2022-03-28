8dea1a083a38e313f3c08784ce35218f14c1d198
Toyemakinwa <toyemakinwa@yahoo.com>
2022-03-28 22:42:57 +0300

    Update script.js
    If added, should correct the syntax errors.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@
     // creating pokemons inside a pokemon list; characters description from pokedex.org
 
     let pokemonList =  [
-        {name: 'Fearrow', height: 1.2, type: ['water', 'speed']}, 
-        {name: 'Ivysaur', height: 1, type: ['grass', 'poison']}, 
-        {name: 'Charmeleon', height: 1.1, type: ['love', 'weather']}];
+        {'name: "Fearrow", height: 1.2, type: ["water", "speed"]'}, 
+        {'name: "Ivysaur", height: 1, type: ["grass", "poison"]'}, 
+        {'name: "Charmeleon", height: 1.1, type: ["love", "weather"]'}
+        ];
 
 </script>
