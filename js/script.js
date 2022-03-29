3a73c9d7d479e72a3746931b22394bfb247e29fc
Toyemakinwa <toyemakinwa@yahoo.com>
2022-03-29 08:34:53 +0300

    Update script.js
    If added, should remove the script tag and correct the code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-<script>
-    // creating pokemons inside a pokemon list; characters description from pokedex.org
 
-    let pokemonList =  [
-        {'name: "Fearrow", height: 1.2, type: ["water", "speed"]'}, 
-        {'name: "Ivysaur", height: 1, type: ["grass", "poison"]'}, 
-        {'name: "Charmeleon", height: 1.1, type: ["love", "weather"]'}
-        ];
+// creating pokemons inside a pokemon list; characters description from pokedex.org
 
-</script>
+ let pokemonList =  [
+    {name: "Fearrow", height: 1.2, type: ["water", "speed"]}, 
+    {name: "Ivysaur", height: 1, type: ["grass", "poison"]}, 
+    {name: "Charmeleon", height: 1.1, type: ["love", "weather"]}
+    ];
+
