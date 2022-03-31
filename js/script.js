329109b9347f259dc4219682f422d41d80027e46
Toyemakinwa <toyemakinwa@yahoo.com>
2022-03-31 09:41:54 +0300

    Update script.js
    If added, optimize the  conditional code

--- js/script.js
+++ js/script.js
@@ -10,9 +10,7 @@
  // writing out the special pokemon's name with their height  
 
 for (let i = 0; i < pokemonList.length; i++){
-    if (pokemonList[i].height <1.2 && pokemonList[i].height >1){
-        document.write( pokemonList[i].name + " (Height: " + pokemonList[i].height + ".)" + "<br>");
-          }else if(pokemonList[i].height >1.1){
+    if(pokemonList[i].height >1.1){
             document.write( pokemonList[i].name + " (Height: " + pokemonList[i].height + ".)" + " - " + " Wow, that/'s big! " + "<br>");
           }else{
             document.write( pokemonList[i].name + " (Height: " + pokemonList[i].height + ".)" + "<br>"); 
