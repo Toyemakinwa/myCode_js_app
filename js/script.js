58268375ec7adad002e1ac6cb971072625c46dff
Toyemakinwa <toyemakinwa@yahoo.com>
2022-04-03 22:30:08 +0300

    Update script.js
    If applied, this should list pokemons using the foreach loop

--- js/script.js
+++ js/script.js
@@ -9,11 +9,21 @@
 
  // writing out the special pokemon's name with their height  
 
-for (let i = 0; i < pokemonList.length; i++){
-    if(pokemonList[i].height >1.1){
-            document.write( pokemonList[i].name + " (Height: " + pokemonList[i].height + ".)" + " - " + " Wow, that/'s big! " + "<br>");
-          }else{
-            document.write( pokemonList[i].name + " (Height: " + pokemonList[i].height + ".)" + "<br>"); 
-        }
-    }
+//for (let i = 0; i < pokemonList.length; i++){
+  //  if(pokemonList[i].height >1.1){
+    //        document.write( pokemonList[i].name + " (Height: " + pokemonList[i].height + ".)" + " - " + " Wow, that/'s big! " + "<br>");
+      //    }else{
+        //    document.write( pokemonList[i].name + " (Height: " + pokemonList[i].height + ".)" + "<br>"); 
+        //}
+   // }
     
+
+
+// writing out pokemons using the foreach loop
+    pokemonList.forEach(function(pokemon) {
+      if(pokemon.height >1.1){
+        document.write( pokemon.name + " (Height: " + pokemon.height + ".)" + " - " + " Wow, that/'s big! " + "<br>");
+      }else{
+        document.write( pokemon.name + " (Height: " + pokemon.height + ".)" + "<br>"); 
+    }
+  });
