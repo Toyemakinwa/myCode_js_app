9030971d1903207601420e27d24489e70a052b54
Toyemakinwa <toyemakinwa@yahoo.com>
2022-04-20 09:21:31 +0300

    updated code
    if commited, this should show a modal content

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 
 // creating pokemons inside a pokemon list; characters description from pokedex.org
 let pokemonRepository = (function () {
+  //first we create a modal container by btargetingnthe class in the HTML
+  let modalContainer= document.querySelector("#modal-container");
   let pokemonList = [];
   // creating the variable to access the API
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
@@ -76,11 +78,63 @@ let pokemonRepository = (function () {
       console.error(e);
     });
   }
+//creating a modal to show title and text
+  function showModal(title, text){
+    // this clears exixting html element
+    modalContainer.innerHTML= "";
+    //we then create a new block for the modal class
+    let modal= document.createElement("div");
+    modal.classList.add("modal");
+
+    // also create a button to close the modal
+    let closeButtonElement= document.createElement("button");
+    closeButtonElement.classList.add("modal-close");
+    closeButtonElement.innerText= "close";
+    closeButtonElement.addEventListener("click", hideModal);
+
+    //Now we create the title element for the modal
+    let titleElement= document.createElement("h1");
+    titleElement.innerText= title;
+
+    //And we also create the contentt element
+    let contentElement= document.createElement("p");
+    contentElement.innerText= text;
+
+    //Now we add it to the HTML
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+    modalContainer.classList.add("is-visible");
+  }
+    //this is to hide the modal container
+    function hideModal() {
+      modalContainer.classList.remove("is-visible");
+    }
+    
+    document.querySelector("#show-modal").addEventListener("click", () => {
+      showModal("modal title", "This is the modal content");
+    });
+    //This also hides the modal container using thee escape button of the keyboard
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "Escape" && modalContainer.classList.contains("is-visible")){
+        hideModal();
+      }
+    });
+
+    //this also hides the modal container when you click outside of it
+    modalContainer.addEventListener("click", (e) => {
+      let target= e.target;
+      if (target === modalContainer){
+        hideModal();
+      }
+    });
 
   // function to show more details about pokemon
   function showDetails(pokemon) {
     pokemonRepository.loadDetails(pokemon).then(function () {
       console.log(pokemon);
+      showModal(pokemon);
     });
   }
 
@@ -90,7 +144,8 @@ let pokemonRepository = (function () {
     addListItem: addListItem,
     loadList: loadList,
     loadDetails: loadDetails,
-    showDetails: showDetails
+    showDetails: showDetails,
+    showModal: showModal
   };
 })();
    
